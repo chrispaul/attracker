d4ee3b670535f493d4e6db55562e1515d89642f9
chrisfpaul@gmail.com <chrisfpaul@gmail.com>
2015-07-20 17:14:09 -0400

    admin

--- attracker_app/admin.py
+++ attracker_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import AppalachianTrail, Segment
+
+admin.site.register(AppalachianTrail)
+
+
+class SegmentAdmin(admin.ModelAdmin):
+    #fields = ['date', 'start_mile', 'end_mile']
+    fieldsets = [
+        (None,               {'fields': ['date']}),
+        ('Miles traveled', {'fields': ['start_mile', 'end_mile']}),
+    ]
+
+admin.site.register(Segment, SegmentAdmin)
