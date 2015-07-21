fb7270caeb8f38187dd271876e7b9192da73b190
chrisfpaul@gmail.com <chrisfpaul@gmail.com>
2015-07-21 18:17:38 -0400

    start hiker detail page

--- attracker_app/views.py
+++ attracker_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
 from .models import AppalachianTrail, Segment, Hiker
 
@@ -8,12 +9,13 @@ def index(request):
     context = {'hikers': hikers}
     return render(request, 'attracker/index.html', context)
 
+def hiker(request, hiker_id):
+    hiker = get_object_or_404(Hiker, pk=hiker_id)
+    return render(request, 'attracker/hiker.html', {'hiker': hiker})
+
 
 
 from django.http import HttpResponse
-
-def hiker(request, hiker_id):
-    return HttpResponse("You're looking at segments for hiker %s." % hiker_id)
 
 def add_segment(request, hiker_id):
     response = "You're adding a segment to hiker %s."
