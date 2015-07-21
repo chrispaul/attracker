03f8e9b53948c6e5c25938a3d579258f13948743
chrisfpaul@gmail.com <chrisfpaul@gmail.com>
2015-07-21 17:57:38 -0400

    trivial index template

--- attracker_app/views.py
+++ attracker_app/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import AppalachianTrail, Segment, Hiker
 
+
 def index(request):
     hikers = Hiker.objects.order_by('trail_name')
-    output = ', '.join([h.trail_name for h in hikers])
-    return HttpResponse(output)
+    context = {'hikers': hikers}
+    return render(request, 'attracker/index.html', context)
+
+
+
+from django.http import HttpResponse
 
 def hiker(request, hiker_id):
     return HttpResponse("You're looking at segments for hiker %s." % hiker_id)
