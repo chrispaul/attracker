34703a833d5490d6d0f9c7a86e2756b4eb7df8a7
chrisfpaul@gmail.com <chrisfpaul@gmail.com>
2015-09-08 15:25:28 -0400

    node script to convert Anand Thakkers JSON to Python constants. Display trail from constants

---
+++ attracker_app/data/convert_json_to_python.js
@@ -0,0 +1,24 @@
+//Convert Anand Thakker's centerline-merged.json AT sequenced coordinates to a Python dictionary
+// JSON AT map: https://github.com/anandthakker/apptrail
+var util=require('util')
+var fs = require('fs');
+
+var obj = JSON.parse(fs.readFileSync('centerline-merged.json', 'utf8'));
+var list = obj['geometry']["coordinates"]
+
+
+console.log( "Writing ../at_coordinates.py")
+var stream = fs.createWriteStream("../at_coordinates.py");
+stream.once('open', function(fd) {
+    //An approximation to the midpoint of the trail.
+    var mid_index = Math.floor(list.length)/2
+    stream.write( "# This file is produced by node.js script: convert_json_to_python.js\n" )
+    stream.write( util.format("MID = {'lat': %s, 'lng':%s}\n", list[mid_index][1], list[mid_index][0]) );
+    stream.write("COORDINATES = [")
+    for (var i = 0; i < list.length; i++) {
+        stream.write( util.format("  {'lat': %s, 'lng':%s},\n", list[i][1], list[i][0]) );
+    }
+    stream.write("]\n")
+    stream.end();
+});
+
