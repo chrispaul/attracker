b8f1bceb23c42e6df09a9a9c8cb33f302903fd96
Chris Paul <cp@om1.com>
2017-06-06 17:13:59 -0400

    static files

--- attracker_project/settings.py
+++ attracker_project/settings.py
@@ -115,6 +115,8 @@ DATE_FORMAT = 'Y-m-d'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = '/static/'
 
